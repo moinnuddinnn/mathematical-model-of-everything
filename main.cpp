f41b9a254160512eae1d846e54f38737c2f4e51e
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-11-15 13:49:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,3 +42,31 @@ class DynamicNumerals {
         u += delta;
     }
 };
+
+
+
+//default constructor implementation
+DynamicNumerals::DynamicNumerals() 
+    : a0(std::numeric_limits<long double>::denorm_min()), u(0.0L) {}
+
+//parameterized constructor implementation
+DynamicNumerals::DynamicNumerals(long double init_a0, long double init_u) 
+    : a0(init_a0), u(init_u) {}
+
+
+int main() {
+    DynamicNumerals x1; //initialized with default smallest a0, u=0
+    DynamicNumerals x2(1e-308, 2); //manually defined dynamic factor
+    x1.display();
+    x2.display();
+
+    //evolve x1
+    x1.evolve(0.5);
+    x1.display();
+
+    //connect two variables
+    DynamicNumerals result = x1.connect(x2);
+    result.display();
+
+    return 0;
+}
