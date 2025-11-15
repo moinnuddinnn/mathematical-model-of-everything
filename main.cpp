533c1e672323bc871b875f024fa4f0d080d1bf27
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-11-15 13:42:00 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <limits>
+
+//mathematical framework represented by a class
+class DynamicNumerals {
+    private:
+    //smallest non-zero non-negitive number(hypothetical)
+    long double a0;
+    //dynamic factor
+    long double u;
+    //Explaination: A dynamic number is a data form that consists of a smallest unit "a0" and a dynamic factor "u". the smallest unit represents the minimal quantifiable value, while the dynamic factor allows for variability and evolution of the number over time or operations wrt to the expansion of the spacetime's metric
+};
